3c3559412a702b9b500d74983ffc035f8bff2c06
Pierre Perinet <pierre.perinet@outlook.com>
2017-09-10 13:03:08 -0600

    c5

---
+++ MankalaHTML/Controllers/DocumentsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace MankalaHTML.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Documents";
+
+            return View();
+        }
+    }
+}
